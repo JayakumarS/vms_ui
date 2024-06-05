fa4d0997d6afa6ab5e15a1caabea32cb3e3b9cb7
Lavanyababu34 <babulavanya4594@gmail.com>
2024-06-05 14:23:21 +0530

    Changes Done

--- src/app/crew/application-properties/familiarization-groups/add-familiarization-groups/add-familiarization-groups.component.ts
+++ src/app/crew/application-properties/familiarization-groups/add-familiarization-groups/add-familiarization-groups.component.ts
@@ -1,4 +1,13 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { HttpServiceService } from 'src/app/auth/http-service.service';
+import { serverLocations } from 'src/app/auth/serverLocations';
+import { CommonService } from 'src/app/common-service/common.service';
+import { FamiliarizationGroupsService } from '../familiarization-groups.service';
 
 @Component({
   selector: 'app-add-familiarization-groups',
@@ -6,10 +15,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-familiarization-groups.component.sass']
 })
 export class AddFamiliarizationGroupsComponent implements OnInit {
-
-  constructor() { }
+  docForm: FormGroup;
+  constructor( private fb: FormBuilder,
+    private commonService: CommonService,
+    public familiarizationGroupsService: FamiliarizationGroupsService,
+    public httpClient: HttpClient,
+    public dialog: MatDialog,
+    private snackBar: MatSnackBar,
+    private serverUrl:serverLocations,
+    private httpService:HttpServiceService,
+    public router: Router) { }
 
   ngOnInit(): void {
+    this.docForm = this.fb.group({
+      code: [""],
+      description: [""],
+      sorting: [""]  
+    });
   }
 
 }
--- src/app/crew/application-properties/familiarization-groups/familiarization-groups.service.ts
+++ src/app/crew/application-properties/familiarization-groups/familiarization-groups.service.ts
@@ -1,9 +1,45 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { HttpServiceService } from 'src/app/auth/http-service.service';
+import { serverLocations } from 'src/app/auth/serverLocations';
+import { UnsubscribeOnDestroyAdapter } from 'src/app/shared/UnsubscribeOnDestroyAdapter';
+import { FamiliarizationGroups } from './familiarization-groups.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class FamiliarizationGroupsService {
+export class FamiliarizationGroupsService extends UnsubscribeOnDestroyAdapter{
 
-  constructor() { }
+  isTblLoading = true;
+  dataChange: BehaviorSubject<FamiliarizationGroups[]> = new BehaviorSubject<FamiliarizationGroups[]>([]);
+  
+  constructor(
+    private httpClient: HttpClient,
+    private serverUrl:serverLocations,
+    private httpService:HttpServiceService
+  ) {
+    super();
+  }
+
+  get data(): FamiliarizationGroups[] {
+    return this.dataChange.value;
+  }
+
+  getList(){
+    let value,url;
+    let list = [{code:"YTS",description:"Balaji",sorting:"Balaji"}];
+    this.isTblLoading = false;
+    this.dataChange.next(list);
+    // this.subs.sink = this.httpService.post<any>(url,value).subscribe(
+    //   (data) => {
+    //     this.isTblLoading = false;
+    //     this.dataChange.next(list);
+    //   },
+    //   (error: HttpErrorResponse) => {
+    //     this.isTblLoading = false;
+    //     console.log(error.name + " " + error.message);
+    //   }
+    // );
+  }
 }
